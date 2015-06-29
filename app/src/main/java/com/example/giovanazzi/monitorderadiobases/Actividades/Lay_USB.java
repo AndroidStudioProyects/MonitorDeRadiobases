51e1bacae30989caaec679f69424174f88492840
Diego Giovanazzi <diegogiovanazzi@gmail.com>
2015-06-29 12:32:22 -0300

    add  library usb

---
+++ app/src/main/java/com/example/giovanazzi/monitorderadiobases/Actividades/Lay_USB.java
@@ -0,0 +1,21 @@
+package com.example.giovanazzi.monitorderadiobases.Actividades;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.example.giovanazzi.monitorderadiobases.R;
+
+/**
+ * Created by Laboratorio on 29/06/2015.
+ */
+public class Lay_USB extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.lay_usb);
+
+
+
+    }
+}
