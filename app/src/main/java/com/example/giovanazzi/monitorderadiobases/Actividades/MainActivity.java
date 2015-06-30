b8ad53de71256dfa91fd9a28e01e4a01e698ad86
Diego Giovanazzi <diegogiovanazzi@gmail.com>
2015-06-30 01:19:09 -0300

    ADD USB FTDI
    NO FUNCIONAL

--- app/src/main/java/com/example/giovanazzi/monitorderadiobases/Actividades/MainActivity.java
+++ app/src/main/java/com/example/giovanazzi/monitorderadiobases/Actividades/MainActivity.java
@@ -1,17 +1,76 @@
 package com.example.giovanazzi.monitorderadiobases.Actividades;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.example.giovanazzi.monitorderadiobases.R;
+import com.ftdi.j2xx.D2xxManager;
+import com.ftdi.j2xx.FT_Device;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    //////////USB+++ ///////////
+    static Context DeviceUARTContext;
+    D2xxManager ftdid2xx;
+    FT_Device ftDev = null;
+    int DevCount = -1;
+    int currentIndex = -1;
+    int openIndex = 0;
+    /*graphical objects*/
+    EditText readText;
+    EditText writeText;
+    Spinner baudSpinner;;
+    Spinner stopSpinner;
+    Spinner dataSpinner;
+    Spinner paritySpinner;
+    Spinner flowSpinner;
+    Spinner portSpinner;
+    ArrayAdapter<CharSequence> portAdapter;
+
+    Button configButton;
+    Button openButton;
+    Button readEnButton;
+    Button writeButton;
+    static int iEnableReadFlag = 1;
+
+    /*local variables*/
+    int baudRate; /*baud rate*/
+    byte stopBit; /*1:1stop bits, 2:2 stop bits*/
+    byte dataBit; /*8:8bit, 7: 7bit*/
+    byte parity;  /* 0: none, 1: odd, 2: even, 3: mark, 4: space*/
+    byte flowControl; /*0:none, 1: flow control(CTS,RTS)*/
+    int portNumber; /*port number*/
+    ArrayList<CharSequence> portNumberList;
+
+
+    public static final int readLength = 512;
+    public int readcount = 0;
+    public int iavailable = 0;
+    byte[] readData;
+    char[] readDataToText;
+    public boolean bReadThreadGoing = false;
+    public readThread read_thread;
+
+    boolean uart_configured = false;
+
+
+
+    /////////USB ---////////
 
     Button btn_Camara;
     @Override
@@ -23,6 +82,25 @@ public class MainActivity extends AppCompatActivity {
         Botones();
 
 
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        DevCount = 0;
+        createDeviceList();
+        if(DevCount > 0)
+        {
+            connectFunction();
+            SetConfig(baudRate, dataBit, stopBit, parity, flowControl);
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        disconnectFunction();
     }
 
     private void LevantarXML(){
@@ -43,6 +121,321 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
+
+    ////////usb +++///////////////////
+
+
+
+    public void createDeviceList()
+    {
+        int tempDevCount = ftdid2xx.createDeviceInfoList(DeviceUARTContext);
+
+        if (tempDevCount > 0)
+        {
+            if( DevCount != tempDevCount )
+            {
+                DevCount = tempDevCount;
+                updatePortNumberSelector();
+            }
+        }
+        else
+        {
+            DevCount = -1;
+            currentIndex = -1;
+        }
+    }
+
+    public void disconnectFunction()
+    {
+        DevCount = -1;
+        currentIndex = -1;
+        bReadThreadGoing = false;
+        try {
+            Thread.sleep(50);
+        }
+        catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        if(ftDev != null)
+        {
+            synchronized(ftDev)
+            {
+                if( true == ftDev.isOpen())
+                {
+                    ftDev.close();
+                }
+            }
+        }
+    }
+
+    public void connectFunction()
+    {
+        int tmpProtNumber = openIndex + 1;
+
+        if( currentIndex != openIndex )
+        {
+            if(null == ftDev)
+            {
+                ftDev = ftdid2xx.openByIndex(DeviceUARTContext, openIndex);
+            }
+            else
+            {
+                synchronized(ftDev)
+                {
+                    ftDev = ftdid2xx.openByIndex(DeviceUARTContext, openIndex);
+                }
+            }
+            uart_configured = false;
+        }
+        else
+        {
+            Toast.makeText(DeviceUARTContext, "Device port " + tmpProtNumber + " is already opened", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        if(ftDev == null)
+        {
+            Toast.makeText(DeviceUARTContext,"open device port("+tmpProtNumber+") NG, ftDev == null", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        if (true == ftDev.isOpen())
+        {
+            currentIndex = openIndex;
+            Toast.makeText(DeviceUARTContext, "open device port(" + tmpProtNumber + ") OK", Toast.LENGTH_SHORT).show();
+
+            if(false == bReadThreadGoing)
+            {
+                read_thread = new readThread(handler);
+                read_thread.start();
+                bReadThreadGoing = true;
+            }
+        }
+        else
+        {
+            Toast.makeText(DeviceUARTContext, "open device port(" + tmpProtNumber + ") NG", Toast.LENGTH_LONG).show();
+            //Toast.makeText(DeviceUARTContext, "Need to get permission!", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void updatePortNumberSelector()
+    {
+        //Toast.makeText(DeviceUARTContext, "updatePortNumberSelector:" + DevCount, Toast.LENGTH_SHORT).show();
+
+        if(DevCount == 2)
+        {
+            portAdapter = ArrayAdapter.createFromResource(DeviceUARTContext, R.array.port_list_2,
+                    R.layout.my_spinner_textview);
+            portAdapter.setDropDownViewResource(R.layout.my_spinner_textview);
+            portSpinner.setAdapter(portAdapter);
+            portAdapter.notifyDataSetChanged();
+            Toast.makeText(DeviceUARTContext, "2 port device attached", Toast.LENGTH_SHORT).show();
+            //portSpinner.setOnItemSelectedListener(new MyOnPortSelectedListener());
+        }
+        else if(DevCount == 4)
+        {
+            portAdapter = ArrayAdapter.createFromResource(DeviceUARTContext, R.array.port_list_4,
+                    R.layout.my_spinner_textview);
+            portAdapter.setDropDownViewResource(R.layout.my_spinner_textview);
+            portSpinner.setAdapter(portAdapter);
+            portAdapter.notifyDataSetChanged();
+            Toast.makeText(DeviceUARTContext, "4 port device attached", Toast.LENGTH_SHORT).show();
+            //portSpinner.setOnItemSelectedListener(new MyOnPortSelectedListener());
+        }
+        else
+        {
+            portAdapter = ArrayAdapter.createFromResource(DeviceUARTContext, R.array.port_list_1,
+                    R.layout.my_spinner_textview);
+            portAdapter.setDropDownViewResource(R.layout.my_spinner_textview);
+            portSpinner.setAdapter(portAdapter);
+            portAdapter.notifyDataSetChanged();
+            Toast.makeText(DeviceUARTContext, "1 port device attached", Toast.LENGTH_SHORT).show();
+            //portSpinner.setOnItemSelectedListener(new MyOnPortSelectedListener());
+        }
+
+    }
+
+
+
+    public void SetConfig(int baud, byte dataBits, byte stopBits, byte parity, byte flowControl)
+    {
+        if (ftDev.isOpen() == false) {
+            Log.e("j2xx", "SetConfig: device not open");
+            return;
+        }
+
+        // configure our port
+        // reset to UART mode for 232 devices
+        ftDev.setBitMode((byte) 0, D2xxManager.FT_BITMODE_RESET);
+
+        ftDev.setBaudRate(baud);
+
+        switch (dataBits) {
+            case 7:
+                dataBits = D2xxManager.FT_DATA_BITS_7;
+                break;
+            case 8:
+                dataBits = D2xxManager.FT_DATA_BITS_8;
+                break;
+            default:
+                dataBits = D2xxManager.FT_DATA_BITS_8;
+                break;
+        }
+
+        switch (stopBits) {
+            case 1:
+                stopBits = D2xxManager.FT_STOP_BITS_1;
+                break;
+            case 2:
+                stopBits = D2xxManager.FT_STOP_BITS_2;
+                break;
+            default:
+                stopBits = D2xxManager.FT_STOP_BITS_1;
+                break;
+        }
+
+        switch (parity) {
+            case 0:
+                parity = D2xxManager.FT_PARITY_NONE;
+                break;
+            case 1:
+                parity = D2xxManager.FT_PARITY_ODD;
+                break;
+            case 2:
+                parity = D2xxManager.FT_PARITY_EVEN;
+                break;
+            case 3:
+                parity = D2xxManager.FT_PARITY_MARK;
+                break;
+            case 4:
+                parity = D2xxManager.FT_PARITY_SPACE;
+                break;
+            default:
+                parity = D2xxManager.FT_PARITY_NONE;
+                break;
+        }
+
+        ftDev.setDataCharacteristics(dataBits, stopBits, parity);
+
+        short flowCtrlSetting;
+        switch (flowControl) {
+            case 0:
+                flowCtrlSetting = D2xxManager.FT_FLOW_NONE;
+                break;
+            case 1:
+                flowCtrlSetting = D2xxManager.FT_FLOW_RTS_CTS;
+                break;
+            case 2:
+                flowCtrlSetting = D2xxManager.FT_FLOW_DTR_DSR;
+                break;
+            case 3:
+                flowCtrlSetting = D2xxManager.FT_FLOW_XON_XOFF;
+                break;
+            default:
+                flowCtrlSetting = D2xxManager.FT_FLOW_NONE;
+                break;
+        }
+
+        // TODO : flow ctrl: XOFF/XOM
+        // TODO : flow ctrl: XOFF/XOM
+        ftDev.setFlowControl(flowCtrlSetting, (byte) 0x0b, (byte) 0x0d);
+
+        uart_configured = true;
+        Toast.makeText(DeviceUARTContext, "Config done", Toast.LENGTH_SHORT).show();
+    }
+
+    public void EnableRead (){
+        iEnableReadFlag = (iEnableReadFlag + 1)%2;
+
+        if(iEnableReadFlag == 1) {
+            ftDev.purge((byte) (D2xxManager.FT_PURGE_TX));
+            ftDev.restartInTask();
+            readEnButton.setText("Read Enabled");
+        }
+        else{
+            ftDev.stopInTask();
+            readEnButton.setText("Read Disabled");
+        }
+    }
+
+    public void SendMessage() {
+        if (ftDev.isOpen() == false) {
+            Log.e("j2xx", "SendMessage: device not open");
+            return;
+        }
+
+        ftDev.setLatencyTimer((byte) 16);
+//		ftDev.purge((byte) (D2xxManager.FT_PURGE_TX | D2xxManager.FT_PURGE_RX));
+
+        String writeData = writeText.getText().toString();
+        byte[] OutData = writeData.getBytes();
+        ftDev.write(OutData, writeData.length());
+    }
+
+    final Handler handler =  new Handler()
+    {
+        @Override
+        public void handleMessage(Message msg)
+        {
+            if(iavailable > 0)
+            {
+                readText.append(String.copyValueOf(readDataToText, 0, iavailable));
+            }
+        }
+    };
+
+    private class readThread  extends Thread
+    {
+        Handler mHandler;
+
+        readThread(Handler h){
+            mHandler = h;
+            this.setPriority(Thread.MIN_PRIORITY);
+        }
+
+        @Override
+        public void run()
+        {
+            int i;
+
+            while(true == bReadThreadGoing)
+            {
+                try {
+                    Thread.sleep(50);
+                } catch (InterruptedException e) {
+                }
+
+                synchronized(ftDev)
+                {
+                    iavailable = ftDev.getQueueStatus();
+                    if (iavailable > 0) {
+
+                        if(iavailable > readLength){
+                            iavailable = readLength;
+                        }
+
+                        ftDev.read(readData, iavailable);
+                        for (i = 0; i < iavailable; i++) {
+                            readDataToText[i] = (char) readData[i];
+                        }
+                        Message msg = mHandler.obtainMessage();
+                        mHandler.sendMessage(msg);
+                    }
+                }
+            }
+        }
+
+    }
+
+
+
+
+
+
+
+
+    //////////// usb ---/////////
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -65,4 +458,8 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
+
+
 }
